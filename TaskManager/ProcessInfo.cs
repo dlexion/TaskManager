0c5d89aaa6d5874ac078e12d9ea6b51682cad3b8
dlexion <dlexion@yandex.ru>
2018-03-07 11:16:22 +0300

    Add info classes

---
+++ TaskManager/ProcessInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    class ProcessInfo
+    {
+        [DisplayName("Name")]
+        public String ProcessName { get; set; }
+
+        [DisplayName("ID")]
+        public int ID { get; set; }
+
+        [DisplayName("Owner")]
+        public String Owner { get; set; }
+
+        [DisplayName("Priority")]
+        public String PriorityClass { get; set; }
+
+        [DisplayName("Memory")]
+        public long PrivateMemorySize64 { get; set; }
+
+        [DisplayName("Threads")]
+        public int NumberOfThreads { get; set; }
+    }
+}
---
+++ TaskManager/ThreadInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    class ThreadInfo
+    {
+        [DisplayName("ID")]
+        public String ID { get; set; }
+
+        [DisplayName("Priority")]
+        public String PriorityLevel { get; set; }
+
+        [DisplayName("State")]
+        public String ThreadState { get; set; }
+    }
+}
